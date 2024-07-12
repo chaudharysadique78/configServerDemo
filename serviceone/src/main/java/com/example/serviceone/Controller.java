d3d03d55a0937be64e4661f1dbfbf2e73e4a00b2
chaudharysadique78 <chaudharysadique78@gmail.com>
2024-07-12 15:17:36 +0530

    remove open feign thats not needed

--- serviceone/src/main/java/com/example/serviceone/Controller.java
+++ serviceone/src/main/java/com/example/serviceone/Controller.java
@@ -1,10 +1,7 @@
 package com.example.serviceone;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +17,14 @@ public class Controller {
     @Value("${location}")
     private String location;
 
+    @Value("${topic}")
+    private String topic;
+
     @GetMapping()
     public String message() {
-        return message+" "+location;
+        return message+" "+location+" "+ topic;
     }
+
+
 
 }
